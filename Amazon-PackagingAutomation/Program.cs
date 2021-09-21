ddbada4f95ff5298f73a0f734aae8c82f5cc3736
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-09-20 22:38:30 -0700

    Add project files.

---
+++ Amazon-PackagingAutomation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Amazon_PackagingAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] arr = new int[] { 1, 1,1, 1 };
+            Console.WriteLine(packagingMine(arr));
+        }
+
+		static int packagingMine(int[] arr)
+		{
+			int[] freq = new int[190000];
+			int max = 0;
+			for (int k = 0; k < arr.Length; ++k)
+			{
+				freq[arr[k]]++;
+				max = Math.Max(max, arr[k]);
+			}
+			int i = 1;
+			int j = 0;
+			while (i <= max)
+			{
+				if (freq[i] > 0 && j < i)
+				{
+					arr[j++] = j;
+				}
+				freq[i]--;
+				if (freq[i] < 1)
+				{
+					i++;
+				}
+			}
+			return arr[j - 1];
+		}
+	}
+}
